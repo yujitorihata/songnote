3155a429239054e3d8a68bf778ab0e23903c2ae8
Yuji.Torihata <yujitorihata@gmail.com>
2015-10-12 21:25:39 +0900

    スタイルの設定＆詳細ページでのタイトル設定

--- songnote2/app/src/main/java/jp/torihata/songnote/Detail1Fragment.java
+++ songnote2/app/src/main/java/jp/torihata/songnote/Detail1Fragment.java
@@ -2,10 +2,10 @@ package jp.torihata.songnote;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,6 +21,11 @@ public class Detail1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // ActionBarのタイトルをセット
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.app_name);
+
+        // 本文のセット
         View v =inflater.inflate(R.layout.fragment_detail1, container, false);
         final TextView body = (TextView)v.findViewById(R.id.body);
         body.setText(getString(R.string.lyrics_ontime));
